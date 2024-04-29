c73de05ba75745fb6bf644f5f41dbc30cb4e3b9c
Hiyuhh <h.mubaidin00@gmail.com>
2024-04-29 16:20:11 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+// Understanding Data Types in JavaScript
+
+
+// Task 1: Declare variables to represent student information such as name, age, grade, and attendance status.
+
+// Task 2: Assign sample values to the student information variables.
+
+// Task 3: Display the student information using console.log() statements.
+
+let name = "Mariam";
+let age = 23;
+let grade = 14;
+let attendance = true;
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Grade: ${grade}`);
+console.log(`Attendance: ${attendance}`);
+
+
+
+
+// Exploring JavaScript Operators
+
+
+// Task 1: Declare variables to store two numeric values for performing arithmetic operations.
+
+// Task 2: Assign sample numeric values to the variables declared in Task 1.
+
+// Task 3: Perform arithmetic operations on your numeric values using various operators and display the results.
+
+// Task 4: Explore assignment operators and update the values of variables.
+
+// Task 5: Use comparison operators to compare the values of variables.
+
+// Task 6: Apply logical operators to combine conditions and display the results.
+
+let num1 = 10;
+let num2 = 5;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log(`Sum: ${sum}`);
+console.log(`Difference: ${difference}`);
+console.log(`Product: ${product}`);
+console.log(`Quotient: ${quotient}`);
+
+num1 -= 5;
+num2 += 2;
+
+console.log(`Updated num1: ${num1}`);
+console.log(`Updated num2: ${num2}`);
+
+let isEqual = num1 === num2;
+let isGreaterThan = num1 > num2;
+let isNotEqual = num1 !== num2;
+
+console.log(`Is num1 equal to num2? ${isEqual}`);
+console.log(`Is num1 greater than num2? ${isGreaterThan}`);
+console.log(`Is num1 not equal to num2? ${isNotEqual}`);
+
+let isPositive = num1 > 0 && num2 > 0;
+let isEven = num1 % 2 == 0 || num2 % 2 == 0;
+
+console.log(`Are both numbers positive? ${isPositive}`);
+console.log(`Is at least one number even? ${isEven}`);
+
+
+
+
